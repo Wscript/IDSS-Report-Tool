337c8db58e47bf781afa9d43dd716dfe54202993
Wscript <dick.li.sheng@gmail.com>
2015-03-13 14:56:26 +0800

    发布第一版

---
+++ IDSS Report Tool/formReportExport.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace IDSS_Report_Tool
+{
+    partial class formReportExport
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.radioNonParameter = new System.Windows.Forms.RadioButton();
+            this.radioHasParameter = new System.Windows.Forms.RadioButton();
+            this.comboReportList = new System.Windows.Forms.ComboBox();
+            this.groupReportType = new System.Windows.Forms.GroupBox();
+            this.labelReportName = new System.Windows.Forms.Label();
+            this.richDescription = new System.Windows.Forms.RichTextBox();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.labelParameter = new System.Windows.Forms.Label();
+            this.textParameter = new System.Windows.Forms.TextBox();
+            this.groupReportType.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioNonParameter
+            // 
+            this.radioNonParameter.AutoSize = true;
+            this.radioNonParameter.Location = new System.Drawing.Point(40, 30);
+            this.radioNonParameter.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.radioNonParameter.Name = "radioNonParameter";
+            this.radioNonParameter.Size = new System.Drawing.Size(83, 24);
+            this.radioNonParameter.TabIndex = 0;
+            this.radioNonParameter.TabStop = true;
+            this.radioNonParameter.Text = "不含参数";
+            this.radioNonParameter.UseVisualStyleBackColor = true;
+            this.radioNonParameter.CheckedChanged += new System.EventHandler(this.radioNonParameter_CheckedChanged);
+            // 
+            // radioHasParameter
+            // 
+            this.radioHasParameter.AutoSize = true;
+            this.radioHasParameter.Location = new System.Drawing.Point(208, 30);
+            this.radioHasParameter.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.radioHasParameter.Name = "radioHasParameter";
+            this.radioHasParameter.Size = new System.Drawing.Size(83, 24);
+            this.radioHasParameter.TabIndex = 1;
+            this.radioHasParameter.TabStop = true;
+            this.radioHasParameter.Text = "包含参数";
+            this.radioHasParameter.UseVisualStyleBackColor = true;
+            this.radioHasParameter.CheckedChanged += new System.EventHandler(this.radioHasParameter_CheckedChanged);
+            // 
+            // comboReportList
+            // 
+            this.comboReportList.FormattingEnabled = true;
+            this.comboReportList.Location = new System.Drawing.Point(9, 139);
+            this.comboReportList.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.comboReportList.Name = "comboReportList";
+            this.comboReportList.Size = new System.Drawing.Size(337, 28);
+            this.comboReportList.TabIndex = 2;
+            this.comboReportList.SelectedIndexChanged += new System.EventHandler(this.comboReportList_SelectedIndexChanged);
+            // 
+            // groupReportType
+            // 
+            this.groupReportType.Controls.Add(this.radioNonParameter);
+            this.groupReportType.Controls.Add(this.radioHasParameter);
+            this.groupReportType.Location = new System.Drawing.Point(9, 11);
+            this.groupReportType.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupReportType.Name = "groupReportType";
+            this.groupReportType.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupReportType.Size = new System.Drawing.Size(335, 74);
+            this.groupReportType.TabIndex = 3;
+            this.groupReportType.TabStop = false;
+            this.groupReportType.Text = "Report 类型";
+            // 
+            // labelReportName
+            // 
+            this.labelReportName.AutoSize = true;
+            this.labelReportName.Location = new System.Drawing.Point(6, 114);
+            this.labelReportName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelReportName.Name = "labelReportName";
+            this.labelReportName.Size = new System.Drawing.Size(86, 20);
+            this.labelReportName.TabIndex = 4;
+            this.labelReportName.Text = "Report 名称";
+            // 
+            // richDescription
+            // 
+            this.richDescription.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richDescription.Location = new System.Drawing.Point(9, 179);
+            this.richDescription.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.richDescription.Name = "richDescription";
+            this.richDescription.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richDescription.Size = new System.Drawing.Size(335, 154);
+            this.richDescription.TabIndex = 6;
+            this.richDescription.Text = "";
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(125, 384);
+            this.buttonExport.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(101, 39);
+            this.buttonExport.TabIndex = 7;
+            this.buttonExport.Text = "导出";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // labelParameter
+            // 
+            this.labelParameter.AutoSize = true;
+            this.labelParameter.Location = new System.Drawing.Point(14, 347);
+            this.labelParameter.Name = "labelParameter";
+            this.labelParameter.Size = new System.Drawing.Size(37, 20);
+            this.labelParameter.TabIndex = 8;
+            this.labelParameter.Text = "参数";
+            // 
+            // textParameter
+            // 
+            this.textParameter.Location = new System.Drawing.Point(57, 343);
+            this.textParameter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textParameter.Name = "textParameter";
+            this.textParameter.Size = new System.Drawing.Size(286, 26);
+            this.textParameter.TabIndex = 9;
+            // 
+            // formReportExport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 436);
+            this.Controls.Add(this.textParameter);
+            this.Controls.Add(this.labelParameter);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.richDescription);
+            this.Controls.Add(this.labelReportName);
+            this.Controls.Add(this.groupReportType);
+            this.Controls.Add(this.comboReportList);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formReportExport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IDSS Report 导出工具";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.groupReportType.ResumeLayout(false);
+            this.groupReportType.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioNonParameter;
+        private System.Windows.Forms.RadioButton radioHasParameter;
+        private System.Windows.Forms.ComboBox comboReportList;
+        private System.Windows.Forms.GroupBox groupReportType;
+        private System.Windows.Forms.Label labelReportName;
+        private System.Windows.Forms.RichTextBox richDescription;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Label labelParameter;
+        private System.Windows.Forms.TextBox textParameter;
+    }
+}
+
---
+++ IDSS Report Tool/formReportExport.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace IDSS_Report_Tool
+{
+    public partial class formReportExport : Form
+    {
+        public formReportExport ()
+        {
+            InitializeComponent ();
+        }
+
+        private void formMain_Load (object sender, EventArgs e)
+        {
+            if (VersionCheck("IDSSReportTool","2.00.20150313") == "Y")
+            {
+                radioNonParameter.Checked = true;
+                this.AcceptButton = buttonExport;
+            }
+            else
+            {
+                MessageBox.Show ("该程序未获得运行许可，请与管理员联系！");
+                Application.Exit ();
+            }
+        }
+
+        private void radioNonParameter_CheckedChanged (object sender, EventArgs e)
+        {
+            GetReportList ();
+        }
+
+        private void radioHasParameter_CheckedChanged (object sender, EventArgs e)
+        {
+            GetReportList ();
+        }
+
+        private void GetReportList ()
+        {
+            string sqlReportList = "";
+            DataTable dtReportList = new DataTable ();
+
+            string consqlserver = ConfigurationManager.ConnectionStrings["CSD_MGTConnectionString"].ToString () + ";Password=CSD;";
+            SqlConnection con = new SqlConnection (consqlserver);
+
+            if (radioNonParameter.Checked == true)
+            {
+                sqlReportList = "SELECT EMQS_ActionName FROM EMQS_GetReport ORDER BY EMQS_ActionName";
+            }
+            else
+            {
+                sqlReportList = "SELECT EMQS_ActionName FROM EMQS_GetReportEX ORDER BY EMQS_ActionName";
+            }
+
+            SqlDataAdapter daReportList = new SqlDataAdapter (sqlReportList, con);
+
+            try
+            {
+                daReportList.Fill(dtReportList);
+                if (dtReportList.Rows.Count >= 1)
+                {
+                    comboReportList.Items.Clear ();
+
+                    for (int i = 0; i < dtReportList.Rows.Count; i++)
+                    {
+                        comboReportList.Items.Add (dtReportList.Rows[i]["EMQS_ActionName"].ToString ());
+                    }
+                }
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.Message);
+            }
+            finally
+            {
+                con.Close ();
+                con.Dispose ();
+                dtReportList.Dispose ();
+                daReportList.Dispose ();
+            }
+        }
+
+        private string VersionCheck (string strProgramName,string strProgramVer)
+        {
+            string sqlRunAllow = "";
+            DataTable dtRunAllow = new DataTable ();
+
+            string consqlserver = ConfigurationManager.ConnectionStrings["CSD_MGTConnectionString"].ToString () + ";Password=CSD;";
+            SqlConnection con = new SqlConnection (consqlserver);
+
+            sqlRunAllow = "SELECT ActiveStatus FROM 程序版本控制 WHERE ProgramName = '" + strProgramName + "' AND ProgramV = '" + strProgramVer + "'";
+
+            SqlDataAdapter daRunAllow = new SqlDataAdapter (sqlRunAllow, con);
+
+            try
+            {
+                daRunAllow.Fill (dtRunAllow);
+                if (dtRunAllow.Rows.Count >= 1)
+                {
+                    return "Y";
+                }
+                else
+                {
+                    return "N";
+                }
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show (msg.Message);
+                return "N";
+            }
+            finally
+            {
+                con.Close ();
+                con.Dispose ();
+                dtRunAllow.Dispose ();
+                daRunAllow.Dispose ();
+            }
+        }
+
+        private void comboReportList_SelectedIndexChanged (object sender, EventArgs e)
+        {
+            if (comboReportList.Text != "")
+            {
+                string sqlReportMemo = "";
+                DataTable dtReportMemo = new DataTable ();
+
+                string consqlserver = ConfigurationManager.ConnectionStrings["CSD_MGTConnectionString"].ToString () + ";Password=CSD;";
+                SqlConnection con = new SqlConnection (consqlserver);
+
+                if (radioNonParameter.Checked == true)
+                {
+                    sqlReportMemo = "SELECT EMQS_ActionMemo FROM EMQS_GetReport WHERE EMQS_ActionName = '" + comboReportList.Text + "' AND	(NOT ([EMQS_ActionMemo] IS NULL))";
+                }
+                else
+                {
+                    sqlReportMemo = "SELECT EMQS_ActionMemo FROM EMQS_GetReportEX WHERE EMQS_ActionName = '" + comboReportList.Text + "' AND	(NOT ([EMQS_ActionMemo] IS NULL))";
+                }
+
+                SqlDataAdapter daReportMemo = new SqlDataAdapter (sqlReportMemo, con);
+
+                try
+                {
+                    daReportMemo.Fill (dtReportMemo);
+                    if (dtReportMemo.Rows.Count >= 1)
+                    {
+                        richDescription.Text = dtReportMemo.Rows[0]["EMQS_ActionMemo"].ToString ();
+                    }
+                    else
+                    {
+                        richDescription.Text = "没有说明！";
+                    }
+                }
+                catch (Exception msg)
+                {
+                    MessageBox.Show(msg.Message);
+                }
+                finally
+                {
+                    con.Close ();
+                    con.Dispose ();
+                    dtReportMemo.Dispose ();
+                    daReportMemo.Dispose ();
+                }
+            }
+        }
+
+        private void buttonExport_Click (object sender, EventArgs e)
+        {
+            if (radioHasParameter.Checked == true & textParameter.Text == "")
+            {
+                MessageBox.Show ("缺少参数！");
+            }
+            else
+            {
+                string sqlReportParameter = "";
+                DataTable dtReportParameter = new DataTable ();
+
+                string consqlserver = ConfigurationManager.ConnectionStrings["CSD_MGTConnectionString"].ToString () + ";Password=CSD;";
+                SqlConnection con = new SqlConnection (consqlserver);
+
+                if (radioNonParameter.Checked == true)
+                {
+                    sqlReportParameter = "SELECT * FROM EMQS_GetReport WHERE EMQS_ActionName = '" + comboReportList.Text + "'";
+                }
+                else
+                {
+                    sqlReportParameter = "SELECT * FROM EMQS_GetReportEX WHERE EMQS_ActionName = '" + comboReportList.Text + "'";
+                }
+
+                SqlDataAdapter daReportParameter = new SqlDataAdapter (sqlReportParameter, con);
+
+                try
+                {
+                    daReportParameter.Fill (dtReportParameter);
+                    if (dtReportParameter.Rows.Count >= 1)
+                    {
+                        CreateReport (dtReportParameter.Rows[0]["EMQS_ServerName"].ToString (),
+                                      dtReportParameter.Rows[0]["EMQS_UID"].ToString (),
+                                      dtReportParameter.Rows[0]["EMQS_DBName"].ToString (),
+                                      dtReportParameter.Rows[0]["EMQS_PWD"].ToString (),
+                                      dtReportParameter.Rows[0]["EMQS_PName"].ToString ());
+                    }
+                    else
+                    {
+                        MessageBox.Show ("没有符合条件的数据！");
+                    }
+                }
+                catch (Exception msg)
+                {
+                    MessageBox.Show (msg.Message);
+                }
+                finally
+                {
+                    con.Close ();
+                    con.Dispose ();
+                    dtReportParameter.Dispose ();
+                    daReportParameter.Dispose ();
+                }
+            }
+        }
+
+        private void CreateReport (string strDataSource, string strUserID, string strInitialCatalog, string strPassword, string PName)
+        {
+            string sqlReportData = "";
+            DataTable dtReportData = new DataTable ();
+
+            string consqlserver = "Data Source=" + strDataSource +
+                                  ";User ID=" + strUserID +
+                                  ";Initial Catalog=" + strInitialCatalog +
+                                  ";Password=" + strPassword;
+            SqlConnection con = new SqlConnection (consqlserver);
+
+            if (radioNonParameter.Checked == true)
+            {
+                sqlReportData = "EXEC " + PName;
+            }
+            else
+            {
+                sqlReportData = "EXEC " + PName + " '" + textParameter.Text.ToString () + "'";
+            }
+
+            SqlDataAdapter daReportData = new SqlDataAdapter (sqlReportData, con);
+
+            try
+            {
+                daReportData.Fill (dtReportData);
+                if (dtReportData.Rows.Count >= 1)
+                {
+                    var xlsReport = new Excel.Application ();
+                    xlsReport.Visible = true;
+                    Excel.Workbook workbookReport = xlsReport.Workbooks.Add ();
+                    Excel.Worksheet worksheetReport = workbookReport.Worksheets.Add();
+                    worksheetReport.Cells.Font.Size = 9;
+                    worksheetReport.Name = comboReportList.Text;
+
+                    object[,] valueReport = new object[dtReportData.Rows.Count, dtReportData.Columns.Count];
+
+                    for (int row = 0; row < dtReportData.Rows.Count; row++)
+                    {
+                        for (int col = 0; col < dtReportData.Columns.Count; col++)
+                        {
+                            valueReport[row, col] = dtReportData.Rows[row][col].ToString();
+                        }
+                    }
+
+                    Excel.Range rangeReport = worksheetReport.Range[worksheetReport.Cells[2, 1], worksheetReport.Cells[dtReportData.Rows.Count+1, dtReportData.Columns.Count]];
+                    rangeReport.Value2 = valueReport;
+
+                    for (int col = 0; col < dtReportData.Columns.Count; col++)
+                    {
+                        worksheetReport.Cells[1, col+1] = dtReportData.Columns[col].ColumnName.ToString();
+                    }
+
+                    worksheetReport.Cells.EntireColumn.AutoFit();
+
+                    workbookReport.SaveAs ("d:\\IDSS Report\\" +
+                                            comboReportList.Text + " " +
+                                            DateTime.Now.Year.ToString () + "-" +
+                                            DateTime.Now.Month.ToString () + "-" +
+                                            DateTime.Now.Day.ToString () + " " +
+                                            DateTime.Now.Hour.ToString () + "." +
+                                            DateTime.Now.Minute.ToString () + "." +
+                                            DateTime.Now.Second.ToString () + ".xlsx");
+                    //MessageBox.Show ("得到符合条件的数据" + dtReportData.Rows.Count.ToString());
+                }
+                else
+                {
+                    MessageBox.Show ("没有符合条件的数据！");
+                }
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show (msg.Message);
+            }
+            finally
+            {
+                con.Close ();
+                con.Dispose ();
+                dtReportData.Dispose ();
+                daReportData.Dispose ();
+            }
+        }
+    }
+}
